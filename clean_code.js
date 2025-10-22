276c389e2cb536897010ee3245523cdc6a51094e
marley195 <marley.wetini@gmail.com>
2025-10-23 02:23:02 +1100

    Add clean_code.js and messy_code.js with basic tax calculation functions

---
+++ clean_code.js
@@ -0,0 +1,3 @@
+function calc(taxableAmount, taxRate) {
+    return taxableAmount + taxableAmount * taxRate}
+    let x = calc(100, 0.2)
---
+++ messy_code.js
@@ -0,0 +1,6 @@
+function calc(a, b) {
+    return a + a * b;
+}
+
+let x = calc(100, 0.2);
+console.log('Result:', x);
